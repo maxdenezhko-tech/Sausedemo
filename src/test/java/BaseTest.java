e5f863e37f1dde32115b477f071da3d93390a3d5
maxdenezhko-tech <maxdenezhko@gmail.com>
2026-06-21 12:59:29 +0300

    Update BaseTest.java
    fix: removed lines

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,11 +5,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-
     WebDriver driver;
 
     @BeforeMethod
-
     public void setUP() {
         FirefoxOptions options = new FirefoxOptions();
         options.addArguments("start-maximized");
@@ -19,9 +17,7 @@ public class BaseTest {
     }
 
     @AfterMethod
-
     public void close() {
         driver.quit();
     }
-
 }
